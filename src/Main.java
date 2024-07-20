13f0ba5b1760ff675570cf98a8812cb96ca714bb
vidyanand <vidyanandme@hotmail.com>
2024-07-20 08:26:33 +0530

    stack reverser

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,13 @@
+import java.util.Scanner;
 import java.util.Stack;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Stack<Integer> stack = new Stack<>();
-        stack.push(10);
-        stack.push(20);
-        stack.push(30);
-        stack.push(40);
-        System.out.println(stack);
-        var top = stack.pop();
-        System.out.println(top);
-        System.out.println(stack.peek());
-        System.out.println(stack);
-        var top2 = stack.pop();
-        System.out.println(stack);
-        System.out.println(top2);// it shows which element is popped
+        String str = "abcd";
+
+        StringReverser reverser = new StringReverser();
+        var result = reverser.reverse(str);
+        System.out.println(result);
+
     }
 }
