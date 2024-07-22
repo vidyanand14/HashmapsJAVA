836c07efee5d1f95a088d9a9413a964e850c79dc
vidyanand <vidyanandme@hotmail.com>
2024-07-22 07:54:25 +0530

    Queue Implimentation

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.Scanner;
 import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
-        String str = "  (a + b)";
-        Expression exp = new Expression();
-        var result = exp.isBalanced(str);
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(10);
+        queue.add(20);
+        queue.add(30);
+        queue.add(40);
+        System.out.println(queue);
+        var result = queue.poll();
         System.out.println(result);
 
 
